33369166e1e7b6d85fd1dd4ab2fb7816ee4e6165
akcan <akcan@DESKTOP-90T78U0>
2022-04-20 00:51:36 +0300

    Proje dosyası ekle.

---
+++ Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace Snake_Game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.timeLabel = new System.Windows.Forms.Label();
+            this.scoreLabel = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Coral;
+            this.panel1.Controls.Add(this.button1);
+            resources.ApplyResources(this.panel1, "panel1");
+            this.panel1.Name = "panel1";
+            // 
+            // button1
+            // 
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.Name = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.DarkOrchid;
+            this.panel2.Controls.Add(this.timeLabel);
+            this.panel2.Controls.Add(this.scoreLabel);
+            resources.ApplyResources(this.panel2, "panel2");
+            this.panel2.Name = "panel2";
+            // 
+            // timeLabel
+            // 
+            resources.ApplyResources(this.timeLabel, "timeLabel");
+            this.timeLabel.ForeColor = System.Drawing.Color.Coral;
+            this.timeLabel.Name = "timeLabel";
+            // 
+            // scoreLabel
+            // 
+            resources.ApplyResources(this.scoreLabel, "scoreLabel");
+            this.scoreLabel.ForeColor = System.Drawing.Color.Coral;
+            this.scoreLabel.Name = "scoreLabel";
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.SystemColors.ControlText;
+            resources.ApplyResources(this.panel3, "panel3");
+            this.panel3.Name = "panel3";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 50;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlText;
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label timeLabel;
+        private System.Windows.Forms.Label scoreLabel;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Snake_Game
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            create(300,300);
+            yem_ozellik();
+            bait_create();
+            timer1.Start();
+        }
+        int count = 0,boyut = 30,yon = 0,x,y,score=0;
+        Label[] lbl=new Label[200];
+        Label yem=new Label();
+        Random random = new Random();
+        Rectangle rect1, rect2;
+        public void create(int LocationX,int LocationY)
+        {            
+            Label a =new Label()
+            {
+                Name = "label"+count,
+                Size = new Size(boyut,boyut),
+                Location = new Point(LocationX,LocationY),
+                BackColor = Color.Red
+            };
+            this.panel3.Controls.Add(a);
+            lbl[count] = a;
+        }
+        void yem_ozellik()
+        {
+            yem.Name = "label";
+            yem.Size = new Size(boyut, boyut);
+            yem.BackColor = Color.Yellow;
+        }
+        public void bait_create()
+        {
+            do
+            {            
+                x = random.Next(0,20)*30;
+                y = random.Next(0, 20)*30;
+                rect1 = new Rectangle(x, y, 20, 20);
+                rect2 = new Rectangle(lbl[0].Location.X, lbl[0].Location.Y, 20, 20);
+            } while (rect1.IntersectsWith(rect2));
+
+            yem.Location = new Point(x, y);
+
+            this.panel3.Controls.Add(yem);
+        }
+        public void yuru()
+        {
+            for (int i = count; i > 0; i--)
+            {
+                lbl[i].Location = lbl[i-1].Location;
+            }
+
+            if (yon == 1)
+            {
+                lbl[0].Location = new Point(lbl[0].Location.X, lbl[0].Location.Y - boyut);
+            }
+            else if(yon == 2)
+            {
+                lbl[0].Location = new Point(lbl[0].Location.X + boyut, lbl[0].Location.Y);
+            }
+            else if (yon == 3)
+            {
+                lbl[0].Location = new Point(lbl[0].Location.X , lbl[0].Location.Y + boyut);
+            }
+            else if (yon == 4)
+            {
+                lbl[0].Location = new Point(lbl[0].Location.X-boyut, lbl[0].Location.Y );
+            }
+            else
+            {
+
+            }
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (yon != 3 && (e.KeyCode == Keys.Up || e.KeyCode == Keys.W))
+            {
+                yon = 1;
+            }
+            else if (yon != 4 && (e.KeyCode == Keys.Right || e.KeyCode == Keys.D))
+            {
+                yon = 2;
+            }
+            else if (yon != 1 && (e.KeyCode == Keys.Down || e.KeyCode == Keys.S))
+            {
+                yon = 3;
+            }
+            else if (yon != 2 && (e.KeyCode == Keys.Left || e.KeyCode == Keys.A)) 
+            {
+                yon = 4;
+            }
+        }
+        public void durum()
+        {
+           Size size = new Size(20,20); 
+           Rectangle aaa = new Rectangle(x, y, 20, 20);
+           Rectangle bbb = new Rectangle(lbl[0].Location,size);
+            Rectangle p = new Rectangle(panel3.Location.X, panel3.Location.Y - boyut * 2, panel3.Width, panel3.Height - 20);            
+            if (!bbb.IntersectsWith(p))
+            {
+                end();
+            }
+            if (aaa.IntersectsWith(bbb))
+            {
+                panel3.Controls.Remove(yem);
+                yılan_buyu();
+                score += 10;
+                scoreLabel.Text = "SCORE : " + score.ToString();
+                bait_create();
+            }
+
+        }
+        private void yılan_buyu()
+        {
+            int a=yem.Location.X, b=yem.Location.Y;
+            count++;
+            create(a,b);
+        }
+        public void end()
+        {
+            timer1.Stop();
+            DialogResult a = MessageBox.Show("Your score : "+ score +"\nDo you want to play again", "GAME OVER",MessageBoxButtons.YesNo);
+            if(a==DialogResult.Yes)
+            {
+                count = 0;
+                yon = 0;
+                score = 0;
+                scoreLabel.Text= "SCORE : "+score.ToString();
+                panel3.Controls.Clear();
+                create(300, 300);
+                yem_ozellik();
+                bait_create();
+                timer1.Start();
+            }
+            else
+            {
+                Application.Exit();
+            }
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            yuru();
+            durum();
+        }
+    }
+}
